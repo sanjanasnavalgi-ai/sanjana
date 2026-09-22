8d15b2faec1f756a3775fd52b6cf2bcbf4610fe8
sanjanasnavalgi-ai <sanjanasnavalgi@gmail.com>
2026-09-22 14:21:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-a=st.number_input("Enter your number")
+a=st.number_input("Enter a no")
 a=st.number_input("Enter another number")
 if st.button("add"):
         st.success(a+b)
@@ -7,3 +7,5 @@ elif st.button("subtract"):
         st.success(a-b)
 elif st.button("multiply"):
         st.success(a*b)
+elif st.button("divide"):
+        st.success(a/b)
