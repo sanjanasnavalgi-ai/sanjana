a5f243e86af3c87b6b0e25d578de53bfdb8ec0c0
sanjanasnavalgi-ai <sanjanasnavalgi@gmail.com>
2026-09-22 12:34:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+a=st.number_input("Enter your number")
+a=st.number_input("Enter another number")
+if st.button("add"):
+        st.success(a+b)
+elif st.button("subtract"):
+        st.success(a-b)
+elif st.button("multiply"):
+        st.success(a*b)
